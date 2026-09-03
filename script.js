362e3f7c2cb5debeb9a09775d19008867158958b
Shredhaya <shredhayagoel@gmail.com>
2026-09-04 00:41:52 +0530

    middleware and ejs

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ import express from 'express'
 
 const app = express()
 
+app.set('view engine', 'ejs');
+
+
 app.use(function (req, res, next) {
     console.log("MIDDLEWARE RUNNING")
 
@@ -15,6 +18,10 @@ app.get('/', (req, res) => {
     console.log("ROUTE RUNNING")
     res.send('Hello World')
 })
+
+app.get('/ejs', (req, res) => {
+    res.render('index', { title: 'ejs', message: "Hello from EJS!", age: 552 })
+});
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000')
